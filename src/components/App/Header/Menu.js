8e139df6a9a7f48990c6e564b631681f68256cc6
Christopher Lee <xchrislee@gmail.com>
2018-03-27 16:46:29 -0700

    Update search prompt in header

--- src/components/App/Header/Menu.js
+++ src/components/App/Header/Menu.js
@@ -3,12 +3,12 @@ import { Dropdown, Icon, Menu, Segment } from 'semantic-ui-react';
 
 const HeaderMenu = () => (
   <div>
-    <Menu attached='top'>
-      <Dropdown item icon='plus' simple>
+    <Menu attached="top">
+      <Dropdown item icon="plus" simple>
         <Dropdown.Menu>
           <Dropdown.Item>
-            <Icon name='dropdown' />
-            <span className='text'>New</span>
+            <Icon name="dropdown" />
+            <span className="text">New</span>
             <Dropdown.Menu>
               <Dropdown.Item>Document</Dropdown.Item>
               <Dropdown.Item>Image</Dropdown.Item>
@@ -22,17 +22,17 @@ const HeaderMenu = () => (
           <Dropdown.Item>Share</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
-      <Menu.Menu position='right'>
-        <div className='ui right aligned category search item'>
-          <div className='ui transparent icon input'>
-            <input className='prompt' type='text' placeholder='Search foods...' />
-            <i className='search link icon' />
+      <Menu.Menu position="right">
+        <div className="ui right aligned category search item">
+          <div className="ui transparent icon input">
+            <input className="prompt" type="text" placeholder="Search games..." />
+            <i className="search link icon" />
           </div>
-          <div className='results' />
+          <div className="results" />
         </div>
       </Menu.Menu>
     </Menu>
   </div>
-)
+);
 
 export default HeaderMenu;
