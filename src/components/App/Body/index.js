a997b3018a63e0330cb0da092d0e773d386e89a4
Christopher Lee <xchrislee@gmail.com>
2018-03-27 18:12:54 -0700

    Implement Flag Icon

--- src/components/App/Body/index.js
+++ src/components/App/Body/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import FlagIcon from '../FlagIcon';
 
 export default class Body extends Component {
-  componentDidMount() {
-    console.log(this, 'body');
-  }
+  // componentDidMount() {
+  //   console.log(this, 'body');
+  // }
 
   renderGameList() {
     return (
@@ -21,7 +22,15 @@ export default class Body extends Component {
             const lowestPrice = game.Prices[lowestPriceRegion] / 100;
             return `$${lowestPrice} + ${lowestPriceRegion}`;
           };
-          return <div key={getNsuid()}>{game.Title} {getPrice()} </div>;
+          const countryCode = game.RegionsSortedByPrice[0].toLowerCase();
+          // countryCode = countryCode.toLowercase();
+
+
+          return (
+            <div key={getNsuid()}>
+              {game.Title} {getPrice()}
+              <FlagIcon code={countryCode} />
+            </div>);
         })}
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,45 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.js',
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'react-hot-loader!babel-loader'
-      },
-      { test: /\.(png|jpg|gif)$/, 
-      loader: 'file-loader?name=./images/[name].[ext]'
-      },
-      { test: /\.css$/,
-        include: /node_modules/,
-        loaders: ['style-loader', 'css-loader'],
-      },
+      loader: 'react-hot-loader!babel-loader',
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      loader: 'file-loader?name=./images/[name].[ext]',
+    },
+    {
+      test: /\.css$/,
+      include: /node_modules/,
+      loaders: ['style-loader', 'css-loader'],
+    },
       // {
       //   test: /\.js$/,
       //   exclude: /node_modules/,
       //   loader: ['babel-loader', 'eslint-loader']
       // },
-    ]
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: __dirname + '/app',
+    path: `${__dirname}/app`,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     proxy: {
       '/api/**': {
         target: 'http://localhost:1234',
         secure: false,
-        changeOrigin: true
-      }
+        changeOrigin: true,
+      },
     },
     contentBase: './public',
     hot: true,
