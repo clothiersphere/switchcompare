da47077dc1051d02a6da83132325ba7b1db395b0
Christopher Lee <xchrislee@gmail.com>
2018-05-20 15:53:24 -0700

    Change header menu from hamburger toggle to tab layout due to UI research

--- src/components/App/Header/index.js
+++ src/components/App/Header/index.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import { Button, Icon, Input, Segment } from 'semantic-ui-react';
-// import HeaderMenu from './Menu';
+import { Menu, Button, Icon, Input, Segment, Image, Checkbox } from 'semantic-ui-react';
+import imgUrl from '../../../../public/images/switch_controller_slice.jpg';
+import SwitchLogo from '../../../../public/images/switch_logo.png';
 
-// class Header extends Component {
-//   render() {
-const Header = ({ showGameSales, showAllGames, searchGames }) => (
-  // const { showSale } = this.props;
-  <div className="HeaderMenu">
-    <Icon name="search" size="large" />
-    <Input
-      className="headerInput"
-      placeholder="Search"
-      onSubmit={e => searchGames(e.target.value)}
-    />
-  </div>
-);
+console.log(imgUrl, 'help');
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 'home',
+    };
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const {
+      showGameSales,
+      showAllGames,
+      searchGames,
+      toggleSidebar,
+      displayOptions,
+    } = this.props;
+
+    const { activeItem } = this.state;
+
+    return (
+      <div className="HeaderMenu">
+        <Menu size="massive" pointing secondary>
+          <Menu.Item
+            name="home"
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item name="all games" active={activeItem === 'all games'} onClick={this.handleItemClick} />
+          <Menu.Item name="on sale" active={activeItem === 'sale'} onClick={this.handleItemClick} />
+          <Menu.Item name="recent releases" active={activeItem === 'recent releases'} onClick={this.handleItemClick} />
+          <Menu.Item name="about" active={activeItem === 'about'} onClick={this.handleItemClick} />
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Input icon="search" placeholder="Search..." />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    );
+  }
+}
 
 export default Header;
+
+// <div className="sortboxes">
+// <Checkbox label="metacritic" />
+// <Checkbox label="percentage" />
+// <Checkbox label="release date" />
+// </div>
+
+// <div className="headerMenuButtons">
+//   <Button.Group>
+//     <Button circular icon="list layout" />
+//     <Button circular icon="block layout" />
+//   </Button.Group>
+// </div>
 
 
 // <Button>Home</Button>
 // <Button onClick={() => showAllGames()}>All Games</Button>
 // <Button onClick={() => showGameSales()}>Sale</Button>
 // <Button>About</Button>
+
+
+// <div>
+// <Button className="sidebarButton" size="large" onClick={() => toggleSidebar()}><Icon size="big" name={sidebarButton} /></Button>
+// </div>
